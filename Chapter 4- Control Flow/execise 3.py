e3dc245ff6a845dedafc0d7c46f08776b0316ce3
abdullahFX90 <114065191+abdullahFX90@users.noreply.github.com>
2022-10-28 22:31:14 +0400

    execise 3
    Alien color #3

---
+++ Chapter 4- Control Flow/execise 3.py
@@ -0,0 +1,8 @@
+alien_color = ("red")
+
+if alien_color == "Red":
+    print("You just earned 5 points")
+elif alien_color == "Red":
+    print("You just earned 10 points")
+else:
+    print("You just earned 15 points")
