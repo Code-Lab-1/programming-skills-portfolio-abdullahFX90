b38f74edf3e49eab6c057b1814a8e76a86bac502
abdullahFX90 <114065191+abdullahFX90@users.noreply.github.com>
2022-10-28 20:35:37 +0400

    execise 5
    change guest list

---
+++ Chapter 3- Data Structures/execise 5.py
@@ -0,0 +1,25 @@
+guests = ['umeer abdullah', 'muntazir waleed', 'danish ayaz']
+
+name = guests[0].title()
+print(name + ", please come to dinner.")
+
+name = guests[1].title()
+print(name + ", please come to dinner.")
+
+name = guests[2].title()
+print(name + ", please come to dinner.")
+
+name = guests[1].title()
+print("\nSorry, " + name + " can't make it to dinner.")
+
+del(guests[1])
+guests.insert(1, 'fasail yousif')
+
+name = guests[0].title()
+print("\n" + name + ", please come to dinner.")
+
+name = guests[1].title()
+print(name + ", please come to dinner.")
+
+name = guests[2].title()
+print(name + ", please come to dinner.")
